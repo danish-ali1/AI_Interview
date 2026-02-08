daf9c5393c1fba722602b434f585b8a49857651f
danish-ali1 <datali4444@gmail.com>
2026-02-08 21:48:53 +0530

    Deploy-ready backend setup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -22,7 +22,8 @@ export const signup = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "Lax",
+      secure:true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -49,7 +50,8 @@ export const login = async (req, res) => {
     const token=jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn: "7d",});
     res.cookie("token",token,{
       httpOnly:true,
-      sameSite:"Lax",
+      secure:true,
+      sameSite: "none",
       maxAge:7*24*60*60*1000,
     });
     return res.status(200).json({ message: "Login successful",user: {id:user._id,name:user.name,email:user.email,role:user.role,},});
